c5b6cb6a49afbf65d082938e9d4264a0071eee11
Mayuki Sawatari <mayuki+github@misuzilla.org>
2020-02-17 17:50:31 +0900

    Add unit tests

--- test/LogicLooper.Test/LogicLooperTest.cs
+++ test/LogicLooper.Test/LogicLooperTest.cs
@@ -50,5 +50,113 @@ namespace LogicLooper.Test
 
             fps.Should().BeInRange(targetFps-2, targetFps);
         }
+
+        [Fact]
+        public async Task Exit()
+        {
+            using var looper = new Cysharp.Threading.LogicLooper.LogicLooper(60);
+
+            var count = 0;
+            await looper.RegisterActionAsync((in LogicLooperActionContext ctx) =>
+            {
+                count++;
+                return false;
+            });
+
+            await Task.Delay(100);
+            count.Should().Be(1);
+        }
+
+        [Fact]
+        public async Task Throw()
+        {
+            using var looper = new Cysharp.Threading.LogicLooper.LogicLooper(60);
+
+            var count = 0;
+            await Assert.ThrowsAsync<Exception>(() => looper.RegisterActionAsync((in LogicLooperActionContext ctx) =>
+            {
+                count++;
+                throw new Exception("Throw from inside loop");
+            }));
+
+            await Task.Delay(100);
+            count.Should().Be(1);
+        }
+
+        [Fact]
+        public async Task CurrentFrame()
+        {
+            using var looper = new Cysharp.Threading.LogicLooper.LogicLooper(60);
+
+            var currentFrame = 0L;
+            await  looper.RegisterActionAsync((in LogicLooperActionContext ctx) =>
+            {
+                currentFrame = ctx.CurrentFrame;
+                return currentFrame != 10;
+            });
+
+            await Task.Delay(100);
+            currentFrame.Should().Be(10);
+        }
+
+        [Fact]
+        public async Task Shutdown_Delay_Cancel()
+        {
+            using var looper = new Cysharp.Threading.LogicLooper.LogicLooper(60);
+
+            var runLoopTask = looper.RegisterActionAsync((in LogicLooperActionContext ctx) => !ctx.CancellationToken.IsCancellationRequested);
+            runLoopTask.IsCompleted.Should().BeFalse();
+
+            var shutdownTask = looper.ShutdownAsync(TimeSpan.FromMilliseconds(500));
+            await Task.Delay(50);
+            runLoopTask.IsCompleted.Should().BeTrue();
+            shutdownTask.IsCompleted.Should().BeFalse();
+
+            await shutdownTask;
+        }
+
+
+        [Fact]
+        public async Task Shutdown_Delay_Cancel_2()
+        {
+            using var looper = new Cysharp.Threading.LogicLooper.LogicLooper(60);
+
+            var count = 0;
+            var runLoopTask = looper.RegisterActionAsync((in LogicLooperActionContext ctx) =>
+            {
+                count++;
+                return !ctx.CancellationToken.IsCancellationRequested;
+            });
+            runLoopTask.IsCompleted.Should().BeFalse();
+
+            var shutdownTask = looper.ShutdownAsync(TimeSpan.FromMilliseconds(500));
+            await Task.Delay(50);
+            runLoopTask.IsCompleted.Should().BeTrue();
+            var count2 = count;
+            shutdownTask.IsCompleted.Should().BeFalse();
+
+            await shutdownTask;
+            count.Should().Be(count);
+        }
+
+        [Fact]
+        public async Task Shutdown_Immediately()
+        {
+            using var looper = new Cysharp.Threading.LogicLooper.LogicLooper(1);
+
+            var signal = new ManualResetEventSlim();
+            var runLoopTask = looper.RegisterActionAsync((in LogicLooperActionContext ctx) =>
+            {
+                signal.Set();
+                return !ctx.CancellationToken.IsCancellationRequested;
+            });
+            runLoopTask.IsCompleted.Should().BeFalse();
+
+            signal.Wait();
+            var shutdownTask = looper.ShutdownAsync(TimeSpan.Zero);
+            await shutdownTask;
+
+            runLoopTask.IsCompleted.Should().BeFalse(); // When the looper thread is waiting for next cycle, the loop task should not be completed.
+        }
     }
 }
